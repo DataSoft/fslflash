d2bee5eb75ee295687350efd46e2236e42d8940e
Aaron Brice <aaron.brice@datasoft.com>
2014-09-16 10:16:35 -0700

    Adding 64 bit dll

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
-import sys
+import platform
 from cx_Freeze import setup, Executable
-
-base = None
-if sys.platform == 'win32':
-    base = 'Win32GUI'
 
 options = {
     'build_exe': {
         'includes': 'atexit',
-        'include_files': 'libusb-1.0.dll',
     }
 }
 
+base = None
+
+arch, os = platform.architecture()
+if os == 'WindowsPE':
+    base = 'Win32GUI'
+    if arch == '64bit':
+        print('Using 64bit libraries')
+        options['build_exe']['include_files'] = 'amd64/libusb-1.0.dll'
+    else:
+        print('Using 32bit libraries')
+        options['build_exe']['include_files'] = 'x86/libusb-1.0.dll'
+
 executables = [
+    Executable('fslflash.py', base=base),
     Executable('fslflashgui.py', base=base)
 ]
 
 setup(name='fslflash',
       version='1.0',
-      description='Vybrid Flash Utility',
+      description='Freescale Vybrid flashing utility',
       options=options,
       executables=executables
       )
